e81e88fd030e456f6a5ae311aa074cad91ef323c
Ira Varshavets <varshavets@gmail.com>
2023-05-27 14:25:30 +0200

    Updated linked list reverse algorithm

--- algorithms/linked_list_reversed.js
+++ algorithms/linked_list_reversed.js
@@ -9,6 +9,7 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
 // Approach 1: Iterative
 // Time complexity : O(n), where n is the list's length.
 // Space complexity : O(1).
@@ -28,7 +29,26 @@ const reverseList = function(head) {
     return previous
 };
 
-// Approach 2: Recursive
+// Approach2: Iteratively adding to a new list
+// Time complexity : O(n)
+// Space complexity : O(n). The extra space for a new list
+const reverseListIterative2 = function(head) {
+  let currentNode = head;
+  const reversedList = new LinkedList();
+  let previous = null
+
+  while (currentNode) {
+    const node = new LinkedListNode(currentNode.value)
+    node.next = previous
+    previous = node
+
+    currentNode = currentNode.next;
+  }
+  reversedList.head = previous
+  return previous;
+}
+
+// Approach 3: Recursive
 // Time complexity : O(n), where n is the list's length.
 // Space complexity : O(n).
 // The extra space comes from implicit stack space due to recursion.
