454896b3950931a6e60b0faa00d6d575d419fb20
Ira Varshavets <varshavets@gmail.com>
2023-06-12 11:08:45 +0200

    Swapping nodes - added edge cases

---
+++ algorithms/linked_list_swap_nodes.js
@@ -0,0 +1,135 @@
+// Task
+// Given a linked list, swap the two nodes present at position i and j, assuming 0 <= i <= j.
+// The positions are based on 0-based indexing.
+
+// @param {ListNode} head
+// @param {positionOne} number
+// @param {positionTwo} number
+// @return {ListNode} head
+
+import {LinkedList} from "../data_structures/LinkedList.js";
+
+const swapNodes = function(head, positionOne, positionTwo) {
+  // invalid inputs
+  if (!head || positionOne < 0 || positionTwo < 0 || positionOne > positionTwo) {
+    return null
+  }
+
+  if (positionOne === positionTwo) {
+    return head
+  }
+
+  let count = 0
+  let current = head
+  let nodeOne = null
+  let prevOne = null
+  let nodeTwo = null
+  let prevTwo = null
+  let nodeOneNext = null
+
+  while(current && count <= positionTwo) {
+    if (count === positionOne - 1) {
+      prevOne = current
+      nodeOne = current.next
+      nodeOneNext = nodeOne.next
+    } else if (count === positionTwo - 1) {
+      prevTwo = current
+      nodeTwo = current.next
+      break
+    }
+
+    current = current.next
+    count += 1
+  }
+
+  // Swap logic
+  // Edge case if list length is smaller than positionOne or Two and no nodeOne or Two is found
+  if (!nodeOne && positionOne !== 0 || !nodeTwo) {
+    return null
+  }
+
+  // The case of updating the head
+  if (positionOne === 0) {
+    // Defining nodes that were not found in the previous loop
+    nodeOne = head
+    nodeOneNext = nodeOne.next
+    // Logic
+    nodeOne.next = nodeTwo.next
+    prevTwo.next = nodeOne
+    nodeTwo.next = nodeOneNext
+    head = nodeTwo
+  }
+  // The case when positionOne and positionTwo are adjacent
+  else if (positionTwo === positionOne + 1) {
+    nodeOne.next = nodeTwo.next
+    nodeTwo.next = nodeOne
+    prevOne.next = nodeTwo
+  } else {
+    nodeOne.next = nodeTwo.next
+    nodeTwo.next = nodeOneNext
+    prevOne.next = nodeTwo
+    prevTwo.next = nodeOne
+  }
+
+  return head
+}
+
+const swapNodes2 = function(head, positionOne, positionTwo) {
+  // invalid inputs
+  if (!head || positionOne < 0 || positionTwo < 0 || positionOne > positionTwo) {
+    return null
+  }
+
+  if (positionOne === positionTwo) {
+    return head
+  }
+
+  let count = 0
+  let current = head
+  let nodeOne = null
+  let prevOne = null
+  let nodeTwo = null
+  let prevTwo = null
+
+  let newHead = null
+
+  while(current) {
+    if (count === positionOne) {
+      nodeOne = current
+    } else if (count === positionTwo) {
+      nodeTwo = current
+      break
+    }
+
+    current = current.next
+    count += 1
+  }
+
+  nodeOne.next = nodeTwo.next
+  nodeTwo.next = nodeOne
+  prevOne.next = nodeTwo
+  prevTwo.next = nodeOne
+
+  return head
+}
+
+const arr1 = [3, 4, 5, 2, 6, 1, 9]
+// const leftIndex = 2
+// const rightIndex = 5
+// Output = [3, 4, 1, 2, 6, 5, 9]
+
+const leftIndex = 0
+const rightIndex = 5
+// Output = [1, 4, 5, 2, 6, 3, 9]
+
+// const leftIndex = 4
+// const rightIndex = 5
+// Output = [3, 4, 5, 2, 1, 6, 9]
+const ll = new LinkedList().fromArray(arr1)
+const result = swapNodes(ll.head, leftIndex, rightIndex)
+
+let node = result
+while(node) {
+  console.log(node.value)
+  node = node.next
+}
