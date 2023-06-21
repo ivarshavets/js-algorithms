aace0b1a3d8d9f0b072f03ac022e8c7179d8e579
Ira Varshavets <varshavets@gmail.com>
2023-06-21 17:15:54 +0200

    max subarray with Divide and conquer algorithm

--- algorithms/array_max_subArray.js
+++ algorithms/array_max_subArray.js
@@ -1,3 +1,7 @@
+// Problem
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
 // Approach: Dynamic Programming, Kadane's Algorithm
 // Time complexity: O(N), where N is the length of nums. We iterate through every element of nums exactly once.
 // Space complexity: O(1). No matter how long the input is, we are only ever using 2 variables: currSum and maxSum.
@@ -16,6 +20,60 @@ const maxSubArrayKadane = function(nums) {
     maxSum = Math.max(maxSum, currSum)
   }
   return maxSum
+};
+
+// Approach: Divide and Conquer
+// This approach is slower than the Kadane's Algorithm and uses more space.
+// Time complexity: O(N⋅logN), where N is the length of nums.
+// On our first call to findBestSubarray, we use for loops to visit every element of nums.
+// Then, we split the array in half and call findBestSubarray with each half.
+// Both those calls will then iterate through every element in that half, which combined is every element of nums again.
+// Then, both those halves will be split in half, and 4 more calls to findBestSubarray will happen,each with a quarter
+// of nums. Every time the array is split, we still need to handle every element of the original input nums.
+// We have to do this logN times since that's how many times an array can be split in half.
+
+// Space complexity: O(logN), where N is the length of nums.
+// The extra space we use relative to input size is solely occupied by the recursion stack.
+// Each time the array gets split in half, another call of findBestSubarray will be added to the recursion stack,
+// until calls start to get resolved by the base case - remember, the base case happens at an empty array,
+// which occurs after logN calls.
+
+const maxSubArrayDivideConquer = function(nums) {
+  const findBestSubArray = function(nums, left, right) {
+    // In case of empty array return negative infinity
+    if (left > right) {
+      return -Infinity
+    }
+
+    let midpoint = Math.floor((left + right) / 2)
+    let currSum = 0
+    let bestLeftSum = 0
+    let bestRightSum = 0
+
+    // Iterate from the middle to the beginning
+    for (let i = midpoint - 1; i >= left; i--) {
+      currSum += nums[i]
+      bestLeftSum = Math.max(bestLeftSum, currSum)
+    }
+
+    // Reset currSum and iterate from the middle to the end.
+    currSum = 0;
+    for (let i = midpoint + 1; i <= right; i++) {
+      currSum += nums[i]
+      bestRightSum = Math.max(bestRightSum, currSum)
+    }
+    // The bestCombinedSum uses the middle element and the best possible sum from each half.
+    let bestCombinedSum = nums[midpoint] + bestLeftSum + bestRightSum
+
+    // Find the best subarray possible from both halves.
+    let leftHalf = findBestSubArray(nums, left, midpoint - 1)
+    let rightHalf = findBestSubArray(nums, midpoint + 1, right)
+
+    // The largest of the 3 is the answer for any given input array.
+    return Math.max(bestCombinedSum, leftHalf, rightHalf)
+  }
+
+  return findBestSubArray(nums, 0, nums.length - 1)
 };
 
 // Approach: Optimized Brute Force
@@ -50,8 +108,7 @@ var maxSubArrayBruteForce = function(nums) {
 };
 
 // Test Cases
-const arr1 = [-2,1,-3,4,-1,2,1,-5,4] // Expected 6
+const arr1 = [-2,1,-3,4,-1,2,1,-5,4] // Expected 6: the subarray [4,-1,2,1] has the largest sum 6.
 const arr2 = [1] // Expected 1
-const arr3 = [5,4,-1,7,8] // Expected 23
+const arr3 = [5,4,-1,7,8] // Expected 23, which is the sum of all elements of the array.
 console.log(maxSubArrayBruteForce(arr1))
-
