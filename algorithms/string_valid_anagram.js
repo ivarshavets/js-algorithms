c7bd1020ac0dd41b5cfa26e5c729a32bc91d17ab
Ira Varshavets <varshavets@gmail.com>
2023-06-28 15:12:47 +0200

    Added algorithm for finding anagram using array

--- algorithms/string_valid_anagram.js
+++ algorithms/string_valid_anagram.js
@@ -3,7 +3,8 @@
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
 // typically using all the original letters exactly once.
 
-const isAnagram = function(s, t) {
+// Approach: Frequency Counter using Map
+const isAnagram_withMap = function(s, t) {
   if (s.length !== t.length) {
     return false
   }
@@ -22,6 +23,36 @@ const isAnagram = function(s, t) {
   return true
 };
 
+// Approach: Frequency Counter using Array
+// Time complexity: O(n).
+// Accessing the counter table is a constant time operation.
+// Space complexity: O(1).
+// Although we do use extra space, the space complexity is O(1)
+// because the table's size stays constant no matter how large n is.
+
+const isAnagram_withArray1 = function(s, t) {
+  if (t.length !== s.length) return false;
+
+  const counter = new Array(26).fill(0);
+
+  for (let i = 0; i < s.length; i++) {
+    // subtract 'a' charCode number (which is equivalent to 97) in order to start counter from 0
+    let sIdx = s[i].charCodeAt(0) - 'a'.charCodeAt(0);
+    let tIdx = t[i].charCodeAt(0) - 'a'.charCodeAt(0);
+    counter[sIdx] = (counter[sIdx] || 0) + 1;
+    counter[tIdx] = (counter[tIdx] || 0) - 1;
+  }
+  // If every element of counter is 0, we found anagram.
+  // If it is greater than 0 it means s has a character whose occurrence is greater than its occurrence in t.
+  // And if it less than 0 then, s has a character whose occurrence is smaller than its occurrence in t.
+  for (let count of counter) {
+    if (count != 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 // Input:
 const s = "anagram"
 const t = "nagaram"
@@ -32,4 +63,4 @@ const t = "nagaram"
 // const t = "car"
 // Output: false
 
-console.log(isAnagram(s,t))
+console.log(isAnagram_withArray1(s,t))
