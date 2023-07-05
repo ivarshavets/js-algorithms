5d973b3d4fceb02a043b166233c8868356c310c0
Ira Varshavets <varshavets@gmail.com>
2023-07-05 19:16:07 +0200

    Added flattening algorithm for doubly linked list by iteration

--- algorithms/doubly_linked_list_flatten.js
+++ algorithms/doubly_linked_list_flatten.js
@@ -33,7 +33,7 @@
 // Space Complexity: O(N). In the worst case, the binary tree might have nodes chained with each other only with
 // the child pointers. In this case, the recursive calls would pile up, and take N space in the function call stack.
 
-var flatten = function(head) {
+const flattenRecurr = function(head) {
     if (!head) {
       return null
     }
@@ -62,4 +62,48 @@ const flattenDFS = function(prev, curr) {
   // remove the child pointer since it is no longer needed in the final result
   curr.child = null
   return flattenDFS(tail, tempNext)
+}
+
+// Approach: DFS by Iteration
+// The stack data structure helps us to construct the iteration sequence as the one created by recursion.
+// The stack here mimics the behavior of the function call stack,
+// so that we could obtain the same result without resorting to recursion.
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+
+var flattenByIteration = function(head) {
+  if (!head) {
+    return null
+  }
+
+  let pseudoHead = new Node(0, null, head, null) // val, prev, next, child
+  let prev = pseudoHead // for tracking prev node
+  let curr = pseudoHead
+
+  let stack = []
+  stack.push(head)
+
+  // enter a loop to iterate the stack until the stack becomes empty.
+  while (stack.length !== 0) {
+    curr = stack.pop(); // removes the last el and returns it
+    // establish the links between prev and curr
+    prev.next = curr;
+    curr.prev = prev;
+
+    // take care of the nodes pointed by the curr.next and curr.child pointers respectively
+    // strictly in this order
+    if (curr.next) {
+      stack.push(curr.next)
+    }
+
+    if (curr.child) {
+      stack.push(curr.child)
+      curr.child = null
+    }
+    prev = curr
+  }
+  // detach the pseudo head node from the result.
+  pseudoHead.next.prev = null
+  return pseudoHead.next
 }
